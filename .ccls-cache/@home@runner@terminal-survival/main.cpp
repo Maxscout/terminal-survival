11ce53a3796261ea880644aa8f407c0ef9fda155
PhillipCeriale <79123338+Maxscout@users.noreply.github.com>
2023-01-24 14:00:57 +0000

    saving works and game compiles. now just making ideas for the start

--- .ccls-cache/@home@runner@terminal-survival/main.cpp
+++ .ccls-cache/@home@runner@terminal-survival/main.cpp
@@ -12,27 +12,6 @@ class Event : public Enemy{ // This has an inheritance of Enemy because event co
 public:
 };
 
-int chksave() { // Checks for save
-	ifstream isave (".saved_game");
-	if(isave.is_open()){return 0;}else{return 1;}
-}
-
-int mksave() {
-	string cmd;
-	ofstream osave (".saved_game");
-	if (osave.is_open()) {
-		cout << "It appears that there isn't a save where this is being executed. Making a new save." << endl;
-		osave << "[gamebeginningv1]\n";
-		osave << "area:newworld\nstate:begin\nstoryline:base\nmodsrequire:base\n";
-		osave << "\n[gameend]";
-		cmd = "gameconditioned";
-		return 0;
-	}else{
-		cout << "You do not have permission to save here!" << endl;
-		return 1;
-	}
-}
-
 int playgame(int startstate) {
 	if (startstate == 0) {
 		return 0;
@@ -42,28 +21,49 @@ int playgame(int startstate) {
 	}
 	return 0;
 }
+class Save { // save system vv
+public:
+string cmd;
+string brand = "base";
+string exist() {
+	cout << "Continue Game?\nPossible commands are :\n start,play,continue,new,backup,quit,exit\n";
+	cout << "> ";
+	cin >> cmd;
+	return cmd;
+	}
+int mksave() {
+	ofstream osave (".saved_game");
+	if (osave.is_open()) {
+		cout << "It appears that there isn't a save where this is being executed. Making a new save." << endl;
+		osave << "[gamebeginningv1]\n";
+		osave << "area:newworld\nstate:begin\nmodsrequire:";
+		osave << brand << endl;
+		osave << "\n[gameend]";
+		osave.close();
+		return 0;
+	}else{
+		cout << "You do not have permission to save here!" << endl;
+		return 1;
+	}
+}
+int chksave() { // Checks for save
+	ifstream isave (".saved_game");
+	if(isave.is_open()){return 0;}else{return 1;}
+}
+};
 
 int main() {
-	string test, cmd, brand;
-	brand = "base";
-	ifstream isave (".saved_game");
 	Player player;
-	if (chksave() == 0) { // Checks if the game found and opened the save
-		cout << "Continue Game?\nPossible commands are :\n start,play,continue,new,backup,quit,exit\n";
-		cout << "> ";
-		cin >> cmd;
-	} else {
-		mksave();
-	} if (cmd == "gameconditioned") {	
+	Save save;
+	string opt;
+	if (save.chksave() == 0) { // Checks if the game found and opened the save
+		save.exist();
+		if (save.cmd == "play" || save.cmd == "start" || save.cmd == "continue")
+		{playgame(1);}
+	} else if (save.mksave() == 0){
 		playgame(0);
-	} else if (cmd == "play" || cmd == "start" || cmd == "continue") {
-		playgame(1);
 	} else {
-		cout << "Invalid cmd!" << endl;
+		return 1;
 	}
-	ofstream osave (".saved_game");
-	osave.close();
-	isave.close();
 	return 0;
 }
-// 69 LINES!!!!!!
---
+++ beginning.md
@@ -0,0 +1,10 @@
+# How will the beginning of this work???
+List of ways this may start:
+
+* You suddenly awaken, in a forest with a headache. You take a look around and find some sticks. what will you do?
+
+Options for this would be:
+
+* take sticks
+* explore,walk around
+* sleep [amt of time here]
