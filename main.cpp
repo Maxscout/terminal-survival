24997f2be0bcc728858df4cf07bff4fb502d8c7f
maxscout09 <doctorrooskie1@Gmail.com>
2023-02-01 19:46:00 +0000

    Trying a new way to do this. It's unsuccessful, press enter to close

--- main.cpp
+++ main.cpp
@@ -6,14 +6,6 @@ using namespace std;
 class Player{ // Controls the actions, logic, and stats of the player in the game
 public:
 };
-class Biome{
-	public:
-		int forest[3];
-		int desert[3];
-		int snowforest[3];
-		int swamp[3];
-		int mountains[3];
-};
 class Enemy{ // This MAY become Event
 public:
 };
@@ -30,6 +22,15 @@ public:
 	string plant[3] = {"5","4","plant"};
 	string cactus[3] = {"6","4","cactus"};
 	string sand[3] = {"7","5","sand"};
+	string snow[3] = {"9","NA","snow"};
+};
+class Biome : public Item{
+	public:
+		string* forestitems[4] = {&stick[1],&stick[3],&tree[1],&tree[3]};
+		string* desertitems[2] = {&cactus[1],&cactus[3]};
+		string* snowforestitems[6] = {&snow[1],&snow[3],&stick[1],&stick[3],&tree[1],&tree[3]};
+		string* swampitems[6] = {&sludge[1],&sludge[3],&tree[1],&tree[3],&stick[1],&stick[3]};
+		string* mountainsitems[2] = {&stone[1],&stone[3]};
 };
 class Save { // save system vv
 public:
@@ -77,35 +78,39 @@ int playgame(int startstate) {
 		ifstream isave(".saved_game");
 		ofstream osave(".saved_game");
 		Item items;
+		Biome biomes;
 		cout << "The chaos is making your head hurt. But you somehow know it will go away.\n";
 		srand(time(NULL));
 		int seed = rand() % 9999999 + 1;
 		srand(seed);
 		osave << "initseed_" << seed << endl;
 		int biome = rand() % 5 + 1;
-		string* blockitem[3];
 		string* itemid[3];
 		osave << "0\n_" << biome << "\n_" << seed << endl;
+		osave << "_";
 		if (biome == 1) {
 			cout << "You awaken as if you were there all along not remembering anything. You see many trees around.\n";
-			blockitem[1] = &items.tree[3];
-			blockitem[2] = &items.stick[3];
-		} if (biome == 2) {
+			osave << *biomes.forestitems[1] << "\n_" << *biomes.forestitems[3];
+			cout << "You found " << *biomes.forestitems[2] << " and " << *biomes.forestitems[4];
+		} else if (biome == 2) {
 			cout << "You're up, very suddenly, standing with sand surrounding every bit of your vision and few cactuses.\n";
-			blockitem[1] = &items.cactus[3];
-		} if (biome == 3) {
+			osave << *biomes.desertitems[1];
+			cout << "You found " << *biomes.desertitems[2];
+		} else if (biome == 3) {
 			cout << "Brrr! It's very cold and theres trees around.\n";
-			blockitem[1] = &items.tree[3];
-		} if (biome == 4) {
+			osave << *biomes.snowforestitems[1] << "\n_" << *biomes.snowforestitems[3] << "\n_" << *biomes.snowforestitems[5];
+			cout << "You found " << *biomes.snowforestitems[2] << ", " << *biomes.snowforestitems[4] << ", and " << *biomes.snowforestitems[6];
+		} else if (biome == 4) {
 			cout << "Yuck! You're knee-deep in mud with some trees that surround you.\n";
-			blockitem[1] = &items.tree[3];
-			blockitem[2] = &items.sludge[3];
-		} if (biome == 5) {
+			osave << *biomes.swampitems[1] << "\n_" << *biomes.swampitems[3] << "\n_" << *biomes.swampitems[5];
+			cout << "You found " << *biomes.swampitems[2] << ", " << *biomes.swampitems[4] << ", and " << biomes.swampitems[6];
+		} else if (biome == 5) {
 			cout << "You're very high in the sky on some mountains. It isn't as cold as you thought.\n";
-			blockitem[1] = &items.stone[3];
+			osave << *biomes.mountainsitems[1];
+			cout << "You found " << *biomes.mountainsitems[2];
 		}
-		osave << "_" << *blockitem[1] << "\n_" << *blockitem[2];
-		cout << "You found " << *blockitem[1] << " and " << *blockitem[2] << endl;
+		cout << ".\nPress enter to close.\n";
+		cin.ignore();
 		osave.close();
 		return 0;
 	} else if (startstate == 1) {
